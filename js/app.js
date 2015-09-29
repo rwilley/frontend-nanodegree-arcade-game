86af6cf51636d011892ba61560f2511cbbdd99f2
Rob <rwilleyart@gmail.com>
2015-09-29 19:11:34 -0400

    make bug and player appear!

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = 2
+    this.y = 3
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -24,12 +25,33 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function (x, y) {
+this.x = x,
+this.y = y, 
+this.sprite = "images/char-boy.png"
+};
 
+Player.prototype.update = function(){
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+//Enemies
+var allEnemies = new Array();
+
+var enemyOne = new Enemy();
+
+allEnemies.push(enemyOne);
+
+//Player
+var player = new Player(1, 2);
 
 
 // This listens for key presses and sends the keys to your
