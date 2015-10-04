078348b89e3193eef4916d6fa5c1d875e13a3d18
Rob <rwilleyart@gmail.com>
2015-10-04 14:32:38 -0400

    Fix this issues and minor style changes from first submission

--- js/app.js
+++ js/app.js
@@ -8,11 +8,10 @@ var Enemy = function(x, y, speed) {
 
 // Update the enemy's position, required method for game
 Enemy.prototype.update = function(dt) {
-    this.y * dt;
     this.x = this.x + this.speed * dt;
-    if (this.x + 100 >= 505) {
+    if (this.x + 100 >= canvasWidth) {
         this.x = -300;
-    }
+    };
 };
 
 // Draw the enemy on the screen, required method for game
@@ -20,11 +19,14 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+var canvasWidth = 505;
+var canvasHeight = 606;
+
 var Player = function (x, y) {
-this.x = x,
-this.y = y,
-speed = 20,
-this.sprite = "images/char-boy.png"
+    this.x = x,
+    this.y = y,
+    speed = 20,
+    this.sprite = 'images/char-boy.png'
 };
 
 Player.prototype.render = function(){
@@ -33,61 +35,61 @@ Player.prototype.render = function(){
 
 Player.prototype.handleInput = function(key){
     switch(key) {
-        case "left":
+        case 'left':
             this.x -= 40;
             break;
-        case "up":
+        case 'up':
             this.y -= 40;
             break;
-        case "right":
+        case 'right':
             this.x += 40;
             break;
-        case "down":
+        case 'down':
             this.y += 40;
             break;
-    }
+    };
 
     //checks boundry
     if(this.x < 0) {
         this.x = 0;
     } else if (this.y < 0) {
         this.y = 0;
-    } else if (this.x + 100 >= 505) {
-        this.x = 505-100;
-    } else if (this.y + 200 >= 606) {
-        this.y = 606 - 200;
-    }
+    } else if (this.x + 100 >= canvasWidth) {
+        this.x = canvasWidth - 100;
+    } else if (this.y + 200 >= canvasHeight) {
+        this.y = canvasHeight - 200;
+    };
 };
 
 Player.prototype.checkCollisions = function() {
     for(var i = 0; i < allEnemies.length; i++) {
-        if (player.x < allEnemies[i].x + 50 &&
-        player.x + 50 > allEnemies[i].x &&
-        player.y < allEnemies[i].y + 50 &&
-        player.y + 50 > allEnemies[i].y) {
-            console.log("collision");
-            alert("Opps, a bug got you. Time to try again.")
-            player.x = 400;
-            player.y = 400;
-        }
-    }
+        if (this.x < allEnemies[i].x + 50 &&
+        this.x + 50 > allEnemies[i].x &&
+        this.y < allEnemies[i].y + 50 &&
+        this.y + 50 > allEnemies[i].y) {
+            console.log('collision');
+            alert('Opps, a bug got you. Time to try again.')
+            this.x = 400;
+            this.y = 400;
+        };
+    };
 };
 
 Player.prototype.checkFinished = function() {
-    if(player.y < 2) {
-        alert("You WIN!")
-        player.x = 400;
-        player.y = 400;
-    }
+    if(this.y < 2) {
+        alert('You WIN!')
+        this.x = 400;
+        this.y = 400;
+    };
 };
 
 Player.prototype.update = function(){
-    Player.prototype.checkCollisions();
-    Player.prototype.checkFinished();
+    this.checkCollisions();
+    this.checkFinished();
 };
 
 //Enemies
-var allEnemies = new Array();
+var allEnemies = [];
 
 var enemyOne = new Enemy(0, 50, 200);
 
